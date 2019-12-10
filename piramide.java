59c613608e1592dda67a83d1da6f8d530e141206
Arnon Henrique Tillmann da Silva <arnon.silva@hbsis.com.br>
2019-12-10 08:18:04 -0300

    Remoção de comentários e imports.

--- piramide.java
+++ piramide.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Random;
 
 public class piramide {
@@ -8,39 +6,39 @@ public class piramide {
 				
 		int bolinhas = 500;
 		int distancia = 35;
-		
 		int dadosCompletos[][] = new int[bolinhas][distancia];
+		int escala[] = new int[bolinhas];
 		
 		Random decisao = new Random();
 		
 		for (int i = 0; i < bolinhas; i++) {
 			int posicionador = 35;
+
 			for (int j = 0; j < distancia; j++) {
 				if(decisao.nextBoolean()) {
+
 					posicionador++;
 					dadosCompletos[i][j] = posicionador;
 					
 				}else {
+
 					posicionador--;
 					dadosCompletos[i][j] = posicionador;
+
 				}									
 			}
 		}
-
-	
-		int escala[] = new int[bolinhas];
-		
-						
+								
 		for (int i = 0; i < dadosCompletos.length; i++) {
-			//int aux = dadosCompletos[i][34]; 
+
 			escala[dadosCompletos[i][34]] += 1;
 		
 		}
 		
 		for (int coluna : escala) {
-			if(coluna > 0) System.out.print(coluna + ";");
-		}
-		
-	}
 
+			if(coluna > 0) System.out.print(coluna + ";");
+
+		}		
+	}
 }
